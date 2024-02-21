dbce239587a519cb718bec2bc940773180856c6a
Tharon Nithiroongrujakorn <tharonn@gmail.com>
2024-02-21 10:32:55 +0800

    Refactor task deletion endpoint

--- app/application/views/tasklist.py
+++ app/application/views/tasklist.py
@@ -56,43 +56,6 @@ def add_task(list_id):
             print(list_id)
             return render_template("index.html", list_id=list_id, form=todoform, checkbox=checkbox, tasks=tasklist, current_user=current_user, errors=todoform.errors)
 
-# @tasklist.route("/tasks", methods=["POST"])
-# def add_task():
-#     if request.method == "POST":
-#         # Validate add task request and turn into a new Task object
-#         form = Todo()
-#         if form.validate():
-#             new_task = Task(
-#                 name=form.name.data,
-#                 date_due=dt.datetime.strptime(request.form["date_due"], "%Y-%m-%dT%H:%M"),
-#                 notes=form.notes.data
-#             )
-
-#             # Retrieve tasklist or create current one
-#             prev_link = request.referrer.split("/")
-#             # Check 4th element (list_id expected location)
-#             if prev_link[-1] != "":
-#                 tasklist = db.session.execute(db.select(TaskList).where(TaskList.listId == prev_link[-1])).scalar()
-#             elif prev_link[-1] == "":
-#                 # Create new list but do not link to author, is now anonymous list
-#                 tasklist = TaskList(
-#                     listId=generate_list_id(),
-#                     author=None
-#                 )
-#                 db.session.add(tasklist)
-#             new_task.tasklist = tasklist
-#             db.session.add(new_task)
-#             db.session.commit()
-
-#             return redirect(url_for("tasklist.show_list", list_id=tasklist.listId))
-#         else:
-#             prev_link = request.referrer.split("/")
-#             print(prev_link)
-#             tasklist = db.session.execute(db.select(TaskList).where(TaskList.listId == prev_link[-1])).scalar()
-#             checkbox = Checkbox()
-#             return redirect(f"/tasks/{prev_link[-1]}")
-#             # return redirect(url_for("tasklist.show_list", list_id=prev_link[-1], errors=form.errors))
-#             # return redirect('index.html', form=form, checkbox=checkbox, tasks=tasklist, current_user=current_user, errors=form.errors)
 
 # TODO fix
 @tasklist.route("/task/<int:task_id>", methods=["POST"])
@@ -116,10 +79,9 @@ def edit_task(task_id):
     return ""
 
 
-# TODO fix
-@tasklist.route("/task/<int:task_id>")
-def delete_task(task_id):
+@tasklist.route("/tasks/<list_id>/<int:task_id>")
+def delete_task(list_id, task_id):
     selected_task = db.get_or_404(Task, task_id)
     db.session.delete(selected_task)
     db.session.commit()
-    return redirect(url_for("show_list", list_id=request.args.get("list_id")))
+    return redirect(url_for("tasklist.show_list", list_id=list_id))
