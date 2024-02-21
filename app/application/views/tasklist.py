10cadd255415ca5db2687199b337c15c0b80d2b5
Tharon Nithiroongrujakorn <tharonn@gmail.com>
2024-02-21 10:20:27 +0800

    Refactor tasklist display and new task adding, template structure

--- app/application/views/tasklist.py
+++ app/application/views/tasklist.py
@@ -7,78 +7,98 @@ import datetime as dt
 
 tasklist = Blueprint("tasklist", __name__, template_folder="templates")
 
-# Anonymous list
+
+# index dud link, redirects to show_list
 @tasklist.route("/")
 def home_page():
-    todoform = Todo()
-    checkbox = Checkbox()
-
-    return render_template("index.html", form=todoform, checkbox=checkbox, current_user=current_user, tasks=None)
+    return redirect(url_for("tasklist.show_list", list_id=generate_list_id()))
 
 
 # Existing user list
-@login_required
-@tasklist.route("/tasks/<list_id>", methods=["GET", "POST"])
+@tasklist.route("/tasks/<list_id>")
 def show_list(list_id):
     todoform = Todo()
     checkbox = Checkbox()
     tasklist = db.session.execute(db.select(TaskList).where(TaskList.listId==list_id)).scalar()
 
+    return render_template("index.html", list_id=list_id, form=todoform, checkbox=checkbox, tasks=tasklist, current_user=current_user)
+
+
+@tasklist.route("/tasks/<list_id>", methods=["POST"])
+def add_task(list_id):
     if request.method == "POST":
-        check = True if request.form["checked"]=="true" else False
-        selected_task = db.get_or_404(Task, request.form["taskid"])
-        selected_task.complete = True if check else False
-        db.session.commit()
-
-    return render_template("index.html", form=todoform, checkbox=checkbox, tasks=tasklist, current_user=current_user)
-
-
-@tasklist.route("/tasks", methods=["POST"])
-def add_task():
-    if request.method=="POST":
-        # referrer is the previous url
-        # check if it was home or show_list
-        # if from home, make new list_id, commit new tasklist
-        prev_link = request.referrer
-        new_tasklist = None
-        if prev_link=="http://localhost:5000/":
-            list_id = generate_list_id()
-            while True:
-                if db.session.execute(db.select(TaskList).where(TaskList.listId==list_id)).scalar() is not None:
-                    list_id = generate_list_id()
-                else:
-                    break
-            new_tasklist = TaskList(
-                listId=generate_list_id(),
-                author=None
+        todoform = Todo()
+        tasklist = db.session.execute(db.select(TaskList).where(TaskList.listId==list_id)).scalar()
+        if todoform.validate():
+            # Create new task
+            new_task = Task(
+                name=todoform.name.data,
+                date_due=todoform.date_due.data,
+                notes=todoform.notes.data
             )
 
-            db.session.add(new_tasklist)
+            # If list does not exist in db, create and save
+            if tasklist is None:
+                tasklist = TaskList(
+                    listId=list_id
+                )
+                db.session.add(tasklist)
+
+            # db relationship linking
+            new_task.tasklist = tasklist
+
+            db.session.add(new_task)
             db.session.commit()
 
-        # get list id
-        selected_id=prev_link[22:]
-        if len(selected_id)==8:
-            selected_list = db.session.execute(db.select(TaskList).where(TaskList.listId==selected_id)).scalar()
+            return redirect(url_for("tasklist.show_list", list_id=tasklist.listId))
         else:
-            selected_list = new_tasklist
+            checkbox = Checkbox()
+            print(list_id)
+            return render_template("index.html", list_id=list_id, form=todoform, checkbox=checkbox, tasks=tasklist, current_user=current_user, errors=todoform.errors)
 
-        new_task = Task(
-            name=request.form["name"],
-            date_due=dt.datetime.strptime(request.form["date_due"], "%Y-%m-%d").date(),
-            notes=request.form["notes"],
-            complete=False,
-            tasklist=selected_list
-        )
-        db.session.add(new_task)
-        db.session.commit()
-        print("added successfully")
-        return redirect(url_for("show_list", list_id=selected_list.listId))
+# @tasklist.route("/tasks", methods=["POST"])
+# def add_task():
+#     if request.method == "POST":
+#         # Validate add task request and turn into a new Task object
+#         form = Todo()
+#         if form.validate():
+#             new_task = Task(
+#                 name=form.name.data,
+#                 date_due=dt.datetime.strptime(request.form["date_due"], "%Y-%m-%dT%H:%M"),
+#                 notes=form.notes.data
+#             )
 
+#             # Retrieve tasklist or create current one
+#             prev_link = request.referrer.split("/")
+#             # Check 4th element (list_id expected location)
+#             if prev_link[-1] != "":
+#                 tasklist = db.session.execute(db.select(TaskList).where(TaskList.listId == prev_link[-1])).scalar()
+#             elif prev_link[-1] == "":
+#                 # Create new list but do not link to author, is now anonymous list
+#                 tasklist = TaskList(
+#                     listId=generate_list_id(),
+#                     author=None
+#                 )
+#                 db.session.add(tasklist)
+#             new_task.tasklist = tasklist
+#             db.session.add(new_task)
+#             db.session.commit()
+
+#             return redirect(url_for("tasklist.show_list", list_id=tasklist.listId))
+#         else:
+#             prev_link = request.referrer.split("/")
+#             print(prev_link)
+#             tasklist = db.session.execute(db.select(TaskList).where(TaskList.listId == prev_link[-1])).scalar()
+#             checkbox = Checkbox()
+#             return redirect(f"/tasks/{prev_link[-1]}")
+#             # return redirect(url_for("tasklist.show_list", list_id=prev_link[-1], errors=form.errors))
+#             # return redirect('index.html', form=form, checkbox=checkbox, tasks=tasklist, current_user=current_user, errors=form.errors)
+
+# TODO fix
 @tasklist.route("/task/<int:task_id>", methods=["POST"])
 def edit_task(task_id):
     db.get_or_404(Task, task_id)
-    if request.method=="POST":
+    if request.method == "POST":
         print(request.form["name"])
         to_edit = db.get_or_404(Task, request.form["id"])
         to_edit.name = request.form["name"]
@@ -89,12 +109,14 @@ def edit_task(task_id):
         # Respond to ajax request
         # Ensure .done overrides once saved
         return jsonify({
-            "name":to_edit.name,
+            "name": to_edit.name,
             "notes": to_edit.notes,
             "datedue": to_edit.date_due
         })
     return ""
 
+
+# TODO fix
 @tasklist.route("/task/<int:task_id>")
 def delete_task(task_id):
     selected_task = db.get_or_404(Task, task_id)
